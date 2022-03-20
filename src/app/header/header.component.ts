2e6847f69d758aff3a8c4a613e64499cded0955d
Pierre Patte <pierre.patte21@orange.fr>
2022-03-20 05:10:21 +0100

    Déplacement compo dans popup

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LgRole } from '../models/lg-role.model';
+import { LgRoles } from '../static/lg-role.static';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   popup!:boolean;
+  popup2!: boolean;
+  roles!: LgRole[];
 
   constructor() {
     this.popup = false;
+    this.popup2 = false;
+    this.roles = LgRoles.LISTE_ALL_ROLES;
    }
 
   ngOnInit(): void {
